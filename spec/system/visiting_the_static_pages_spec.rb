11963a2634962cc76f94b512aedd657f879d5170
thatrandomadmin <thatrandomdev@gmail.com>
2021-12-14 15:01:24 +0000

    linting

--- spec/system/visiting_the_static_pages_spec.rb
+++ spec/system/visiting_the_static_pages_spec.rb
@@ -7,14 +7,17 @@ RSpec.feature 'User visits static pages' do
     visit '/'
     expect(page).to have_title 'Home | Ruby on Rails Tutorial Sample App'
   end
+
   scenario 'User visits the about page' do
     visit '/static_pages/about'
     expect(page).to have_title 'About | Ruby on Rails Tutorial Sample App'
   end
+
   scenario 'User visits the contact page' do
     visit '/static_pages/contact'
     expect(page).to have_title 'Contact | Ruby on Rails Tutorial Sample App'
   end
+
   scenario 'User visits the help page' do
     visit '/static_pages/help'
     expect(page).to have_title 'Help | Ruby on Rails Tutorial Sample App'
