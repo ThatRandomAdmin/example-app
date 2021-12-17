38f05c222297944183d00a34943d31a6b46e8ca0
thatrandomadmin <thatrandomdev@gmail.com>
2021-12-17 17:24:32 +0000

    add rspec test for login, and customised the page a bit

--- spec/system/visiting_the_login_spec.rb
+++ spec/system/visiting_the_login_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature 'User visits login' do
   scenario 'User visits the login page' do
     visit login_path
-    expect(page).to have_title 'Login | Ruby on Rails Tutorial Sample App'
+    expect(page).to have_title 'Log in | Ruby on Rails Tutorial Sample App'
   end
 end
