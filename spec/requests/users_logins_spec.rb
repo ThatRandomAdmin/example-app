0f11910f9c179f59bab9053b51be60d04e3a1c90
thatrandomadmin <thatrandomdev@gmail.com>
2021-12-17 18:00:54 +0000

    linting

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "UsersLogins", type: :request do
-  scenario "login with invalid information" do
+RSpec.describe 'UsersLogins', type: :request do
+  scenario 'login with invalid information' do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: 'defaultemail@example.com', password: "invalid" } }
+    post login_path, params: { session: { email: 'defaultemail@example.com', password: 'invalid' } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
