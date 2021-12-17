731330c1d85175711e1b22b7bd9357618c5d89b6
thatrandomadmin <thatrandomdev@gmail.com>
2021-12-17 17:56:45 +0000

    added some tests for new login

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "UsersLogins", type: :request do
   scenario "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: 'defaultemail@example.com', password: "invalid" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
